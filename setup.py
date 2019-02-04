298ec7edfd3a3f1959a92d6f8800de19b700e013
Valerii Vorobiov <ValeriyVorobiov@gmail.com>
2019-02-04 15:05:34 +0200

    dist files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pew",
+    version="0.0.1",
+    author="Valerii Vorobiov",
+    tests_require=['pytest'],
+    author_email="author@example.com",
+    description="project for book places in coworking.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sucre-Ray/pew",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
